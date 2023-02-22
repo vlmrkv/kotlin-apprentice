4ad5f7768d384506776a868b4123e7f0e685ecf6
Dekram <madxtreme@gmail.com>
2023-02-23 00:56:43 +0300

    Add challenge-2

---
+++ src/main/kotlin/challenge-2.kt
@@ -0,0 +1,43 @@
+import kotlin.reflect.typeOf
+
+fun main() {
+    // #1 Create a constant called 'coordinates' and assign a pair containing two and three to it.
+    val coordinates = Pair(2, 3)
+    // #2 Extract the values 2 and 3 from 'coordinates' into two variables called 'row' and 'column'.
+    var row = coordinates.first
+    var column = coordinates.second
+    // #3 Which of the following are valid statements?
+    val character1: Character = "Dog" // not valid. the type doesn't match the value
+    val character2: Char = 'd' // valid. the type matches the value
+    val string1: String = "Dog" // valid. the type matches the value
+    val string2: String = 'd' // not valid. the type doesn't match the value
+    // #4 Is this valid code?
+    val date = Triple(15, 8, 2015)
+    val day = date.First // not valid
+    val day = date.first // valid
+    // #5 What is wrong with the following code?
+    val name = "Joe"
+    name += " Howard" // val cannot be reassigned
+    // #6 What is the type of the constant named 'value'?
+    val triple = Triple(100, 1.5, 10)
+    val value = triple.second // Using ctrl+shift+P the type of 'value' is Double
+    // #7 What is the value of the constant named 'month'?
+    val newDate = Triple(15, 8, 2015)
+    val month = newDate.second // Using ctrl+shift+P the type of 'month' is Int
+    // #8 What is the value of the constant named 'summary'?
+    val number = 10
+    val multiplier = 10
+    val summary = "$number multiplied by $multiplier equals ${number * multiplier}"
+    println(summary) // 100
+    // #9 What is the sum of a and b, minus c? What is its type?
+    val a = 4
+    val b: Short = 100
+    val c: Byte = 12
+    val sum = a + b - c
+    println(sum) // 92. Type is Int
+    // #10 What is the numeric difference between kotlin.math.PI and kotlin. math.PI.toFloat()?
+    val pi = kotlin.math.PI
+    println(pi) // 3.141592653589793
+    val pi2 = kotlin.math.PI.toFloat()
+    println(pi2) // 3.1415927
+}
