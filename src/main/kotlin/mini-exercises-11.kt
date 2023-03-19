fb2618a733969c17b1c8a8920732e2cf6d718f24
Dekram <madxtreme@gmail.com>
2023-03-19 14:30:47 +0300

    Edit mini-exercises-11

--- src/main/kotlin/mini-exercises-11.kt
+++ src/main/kotlin/mini-exercises-11.kt
@@ -14,4 +14,14 @@ fun main() {
     // equal now?
     val parseInt2 = "cat".toIntOrNull()
     println(parseInt2) // null
+
+    // 4. Using your 'myFavoriteSong' variable, use a null check and smart cast to check if it contains
+    // a value. If it does, print out the value. If it doesn't, print "I don't have a favorite song."
+    if (myFavoriteSong != null) {
+        println("My favorite song is $myFavoriteSong")
+    } else {
+        println("I don't have a favorite song.")
+    }
+    // 5. Change 'myFavoriteSong' to the opposite of what it is now. If it's 'null', set it to a string;
+    // if it's a string, set it to 'null'. Observe how your printed result changes
 }
