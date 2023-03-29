d5936d9fb2c92343aee57d6cdf7a1bd790312842
Dekram <madxtreme@gmail.com>
2023-03-29 22:50:39 +0300

    Solved second task

--- src/main/kotlin/challenge-7.kt
+++ src/main/kotlin/challenge-7.kt
@@ -1,5 +1,3 @@
-import kotlin.random.Random
-
 fun main() {
     // 1. Which of the following 1-10 are valid statements?
     val array1 = Array<Int>() // not valid - required values for parameter 'size' and 'init'
@@ -17,41 +15,56 @@ fun main() {
     array5 += 6 // not valid - val can't be reassigned
     for item in array5 { println(item) } // not valid - a 'for' loop must contain parentheses
 
-    // 2. Write a function that removes the first occurrence of a given integer from a list of
-    // integers. This is the signature of the function:
-    fun removeOne(item: Int, list: List<Int>): List<Int> {
+    val list: List<Int> = mutableListOf(1, 2, 3, 4, 2)
+    val item = 2
+    println(removeOne(item, list).joinToString())
 
-    }
-
-    // 3. Write a function that removes all occurrences of a given integer from a list of integers.
-    // This is the signature of the function:
-    fun remove(item: Int, list: List<Int>): List<Int> {
-
-    }
-
-    // 4. Arrays and lists have a 'reverse()' method that reverses all the elements in-place,
-    // that is, within the original array or list. Write a function that does a similar thing,
-    // without using 'reverse()', and returns a new array with the elements of the original array
-    // in reverse order. This is the signature of the function:
-    fun reverse(array: Array<Int>): Array<Int> {
-
-    }
-
-    // 5. The function below returns a random number between 'from' (inclusive) and the 'to' (exclusive):
-    val random = Random()
-    fun rand(from: Int, to: Int): Int {
-        return random.nextInt(to - from) + from
-    }
-    // Use it to write a function that shuffles the elements of an array in random order. This is the
-    // signature of the function:
-    fun randomized(array: Array<Int>): Array<Int> {
-
-    }
-
-    // 6. Write a function that calculates the minimum and maximum value in an array of integers.
-    // Calculate these values yourself; don't use the methods 'min' and 'max'. Return 'null' if the
-    // given array is empty. This is the signature of the function:
-    fun minMax(numbers: Array<Int>): Pair<Int, Int>? {
-
-    }
 }
+
+// 2. Write a function that removes the first occurrence of a given integer from a list of
+// integers. This is the signature of the function:
+fun removeOne(item: Int, list: List<Int>): List<Int> {
+    var result = listOf<Int>()
+    if (list.contains(item)) {
+        result = list.minus(item)
+    }
+    return result
+}
+
+// 3. Write a function that removes all occurrences of a given integer from a list of integers.
+// This is the signature of the function:
+//fun remove(item: Int, list: List<Int>): List<Int> {
+//    var result = listOf<Int>()
+//    for (i in 0 .. list.lastIndex) {
+//        if (list[i] == item) {
+//            result = list.minus(list[i])
+//        }
+//    }
+//    return result
+//}
+
+// 4. Arrays and lists have a 'reverse()' method that reverses all the elements in-place,
+// that is, within the original array or list. Write a function that does a similar thing,
+// without using 'reverse()', and returns a new array with the elements of the original array
+// in reverse order. This is the signature of the function:
+fun reverse(array: Array<Int>): Array<Int> {
+    return array
+}
+
+// 5. The function below returns a random number between 'from' (inclusive) and the 'to' (exclusive):
+val random = 2
+//fun rand(from: Int, to: Int): Int {
+//    return random.nextInt(to - from) + from
+//}
+// Use it to write a function that shuffles the elements of an array in random order. This is the
+// signature of the function:
+//fun randomized(array: Array<Int>): Array<Int> {
+//
+//}
+
+// 6. Write a function that calculates the minimum and maximum value in an array of integers.
+// Calculate these values yourself; don't use the methods 'min' and 'max'. Return 'null' if the
+// given array is empty. This is the signature of the function:
+//fun minMax(numbers: Array<Int>): Pair<Int, Int>? {
+//
+//}
