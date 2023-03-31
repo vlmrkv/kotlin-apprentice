fa8b9dafda5d6fcf2f1b056e3b4b9bf518b661c7
Dekram <madxtreme@gmail.com>
2023-03-31 22:29:07 +0300

    Solved 5th and 6th tasks

--- src/main/kotlin/challenge-7.kt
+++ src/main/kotlin/challenge-7.kt
@@ -1,30 +1,32 @@
 import java.util.Random
 
 fun main() {
-    // 1. Which of the following 1-10 are valid statements?
-    val array1 = Array<Int>() // not valid - required values for parameter 'size' and 'init'
-    val array2 = arrayOf() // not valid - required reified type
-    val array3: Array<String> = arrayOf() // valid
-    // For the next three statements, 'array4' has been declared as:
-    val array4 = arrayOf(1, 2, 3)
-    println(array4[0]) // valid
-    println(array4[5]) // not valid - index out of bounds
-    array4[0] = 4 // valid
-    // For the final five statements, 'array5' has been declared as:
-    val array5 = arrayOf(1, 2, 3)
-    array5[0] = array5[1] // valid - elements of array can be reassigned
-    array5[0] = "Six" // not valid - 'array5' has type Int
-    array5 += 6 // not valid - val can't be reassigned
-    for item in array5 { println(item) } // not valid - a 'for' loop must contain parentheses
+//    // 1. Which of the following 1-10 are valid statements?
+//    val array1 = Array<Int>() // not valid - required values for parameter 'size' and 'init'
+//    val array2 = arrayOf() // not valid - required reified type
+//    val array3: Array<String> = arrayOf() // valid
+//    // For the next three statements, 'array4' has been declared as:
+//    val array4 = arrayOf(1, 2, 3)
+//    println(array4[0]) // valid
+//    println(array4[5]) // not valid - index out of bounds
+//    array4[0] = 4 // valid
+//    // For the final five statements, 'array5' has been declared as:
+//    val array5 = arrayOf(1, 2, 3)
+//    array5[0] = array5[1] // valid - elements of array can be reassigned
+//    array5[0] = "Six" // not valid - 'array5' has type Int
+//    array5 += 6 // not valid - val can't be reassigned
+//    for item in array5 { println(item) } // not valid - a 'for' loop must contain parentheses
 
     val list: List<Int> = listOf(1, 2, 3, 4, 2)
     val item = 2
     println(removeOne(item, list).joinToString())
     println(remove(item, list).joinToString())
 
-    val array = arrayOf(1, 2, 3, 4, 5)
+    val array = arrayOf<Int>(1, 2, 3, 4, 5)
     println(array.joinToString())
     println(reverse(array).joinToString())
+    println(randomized(array).joinToString())
+    println(minMax(array))
 }
 
 // 2. Write a function that removes the first occurrence of a given integer from a list of
@@ -69,13 +71,34 @@ fun rand(from: Int, to: Int): Int {
 }
 // Use it to write a function that shuffles the elements of an array in random order. This is the
 // signature of the function:
-//fun randomized(array: Array<Int>): Array<Int> {
-//
-//}
+fun randomized(array: Array<Int>): Array<Int> {
+    var result = arrayOf<Int>()
+    val from = 0
+    val to = array.size - 1
+    for (i in from .. to) {
+        val el = rand(from, to)
+        result += el
+    }
+    return result
+}
 
 // 6. Write a function that calculates the minimum and maximum value in an array of integers.
 // Calculate these values yourself; don't use the methods 'min' and 'max'. Return 'null' if the
 // given array is empty. This is the signature of the function:
-//fun minMax(numbers: Array<Int>): Pair<Int, Int>? {
-//
-//}
+fun minMax(numbers: Array<Int>): Pair<Int, Int>? {
+    if (numbers.isEmpty()) {
+        return null
+    } else {
+        var minimum: Int = numbers[0]
+        var maximum: Int = numbers[0]
+        for (i in 0 .. numbers.lastIndex) {
+            if (numbers[i] < minimum) {
+                minimum = numbers[i]
+            }
+            if (numbers[i] > maximum) {
+                maximum = numbers[i]
+            }
+        }
+        return Pair(minimum, maximum)
+    }
+}
