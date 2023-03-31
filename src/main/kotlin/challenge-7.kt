824fe620e48b9520d0790594d92c8984418d4211
Dekram <madxtreme@gmail.com>
2023-03-31 22:31:34 +0300

    Minor fix

--- src/main/kotlin/challenge-7.kt
+++ src/main/kotlin/challenge-7.kt
@@ -1,21 +1,21 @@
 import java.util.Random
 
 fun main() {
-//    // 1. Which of the following 1-10 are valid statements?
-//    val array1 = Array<Int>() // not valid - required values for parameter 'size' and 'init'
-//    val array2 = arrayOf() // not valid - required reified type
-//    val array3: Array<String> = arrayOf() // valid
-//    // For the next three statements, 'array4' has been declared as:
-//    val array4 = arrayOf(1, 2, 3)
-//    println(array4[0]) // valid
-//    println(array4[5]) // not valid - index out of bounds
-//    array4[0] = 4 // valid
-//    // For the final five statements, 'array5' has been declared as:
-//    val array5 = arrayOf(1, 2, 3)
-//    array5[0] = array5[1] // valid - elements of array can be reassigned
-//    array5[0] = "Six" // not valid - 'array5' has type Int
-//    array5 += 6 // not valid - val can't be reassigned
-//    for item in array5 { println(item) } // not valid - a 'for' loop must contain parentheses
+    // 1. Which of the following 1-10 are valid statements?
+    val array1 = Array<Int>() // not valid - required values for parameter 'size' and 'init'
+    val array2 = arrayOf() // not valid - required reified type
+    val array3: Array<String> = arrayOf() // valid
+    // For the next three statements, 'array4' has been declared as:
+    val array4 = arrayOf(1, 2, 3)
+    println(array4[0]) // valid
+    println(array4[5]) // not valid - index out of bounds
+    array4[0] = 4 // valid
+    // For the final five statements, 'array5' has been declared as:
+    val array5 = arrayOf(1, 2, 3)
+    array5[0] = array5[1] // valid - elements of array can be reassigned
+    array5[0] = "Six" // not valid - 'array5' has type Int
+    array5 += 6 // not valid - val can't be reassigned
+    for item in array5 { println(item) } // not valid - a 'for' loop must contain parentheses
 
     val list: List<Int> = listOf(1, 2, 3, 4, 2)
     val item = 2
